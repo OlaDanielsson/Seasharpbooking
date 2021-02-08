c91294caa5895cfa86ce3b86f1a2dab19f265071
Ola Danielsson <danielsson.ola@hotmail.com>
2021-02-08 16:00:29 +0100

    Revert "forts 2"
    This reverts commit c18601df6718cfd5b5dcc0fc3991a7fd54a01c57.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace Seasharpbooking.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)] //Fortsättning 2
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
