124f5582c8bfefd0f5005cbd6f0e273837f50b6b
Hampus Back <haba0018@student.hv.se>
2021-02-09 15:41:26 +0100

    Jag har skapat dessa fina modeller!

---
+++ Models/BookingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seasharpbooking.Models
+{
+    public class BookingModel
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int RoomId { get; set; }
+        public int GuestId { get; set; } //hur fan skriver man denna kopplingen då den ska hämtas från annan API?
+        public RoomModel Room { get; set; } //koppling till Room
+    }
+}
---
+++ Models/RoomModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seasharpbooking.Models
+{
+    public class RoomModel
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int Floor { get; set; }
+        public int RoomNumber { get; set; }
+        public CategoryModel Category { get; set; } //koppling till Category
+    }
+}
