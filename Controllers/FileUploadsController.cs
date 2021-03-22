1d3942ebfcbc28dd6ccd78affdabde2086bd9a29
Hampus Back <haba0018@student.hv.se>
2021-03-22 13:50:06 +0100

    Ändrat alla visuella ord till svenska språket!

---
+++ Controllers/FileUploadsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Seasharpbooking.Controllers
+{
+    public class FileUploadsController : Controller
+    {
+        public IActionResult Post()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<string> Post(IFormFile image)
+        {
+
+            HttpContent fileStreamContent = new StreamContent(image.OpenReadStream());
+            var multiContent = new MultipartFormDataContent
+            {
+                    { fileStreamContent, "image", image.FileName }
+            };
+
+            var response = await ApiConnection.ApiClient.PostAsync("FileUploads", multiContent);
+            var data = await response.Content.ReadAsStringAsync(); ;
+            return data;
+        }
+    }
+}
---
+++ Models/FileUpload.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seasharpbooking.Models
+{
+    public class FileUpload
+    {
+        [NotMapped]
+        public IFormFile image { get; set; }
+    }
+}
